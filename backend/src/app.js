aa9ea1a58f1c0085620fe3cd5f1c417f729b3945
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2021-02-18 02:10:39 +0530

    routes added to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,4 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const userRouter = require('./routes/user');
+const targetRouter = require('./routes/target');
+const historyRouter = require('./routes/history');
+
+app.use('/user', userRouter);
+app.use('/target', targetRouter);
+app.use('/history', historyRouter);
+
 module.exports = app;
